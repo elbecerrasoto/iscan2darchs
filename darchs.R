0be6d6be312d924b718ae06fa32d503c9c16fe92
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-08-21 13:58:14 -0700

    t-SNE

--- darchs.R
+++ darchs.R
@@ -132,3 +132,109 @@ bad <- archs_pid |>
 good <- archs_pid |>
   filter(str_detect(arch, endo) |
     (str_detect(arch, lxg) & str_detect(arch, deam)))
+
+good <- good |>
+  mutate(slength2 = standarize(length),
+         deam = str_detect(arch_code, "å"))
+
+good <- good |>
+  group_by(deam) |>
+  mutate(slength_deam = standarize(length))
+
+
+good <- good |>
+  select(-slength, -slength2) |>
+  rename( slength = slength_deam)
+
+view(good)
+
+gen_type()
+
+x <- archs_pid$arch
+Bdeam <- str_detect(x, deam)
+Blxg <- str_detect(x, lxg)
+Bendo <- str_detect(x, endo)
+Bdeam_lxg <- Bdeam & Blxg
+
+type_pid <- case_when(
+  Bdeam_lxg ~ "Deam_LXG",
+  Bdeam ~ "deam",
+  Bendo ~ "EndoV",
+  .default = "."
+)
+
+archs_pid <- archs_pid |>
+  mutate(type = type_pid)
+
+archs_pid |>
+  left_join(a)
+
+blasts <- read_tsv("blasts.tsv")
+names(blasts)
+archs_pid <- blasts |>
+  distinct(pid, bitscore) |>
+  right_join(archs_pid, join_by(pid))
+
+
+archs_pid <- archs_pid |>
+  mutate(sbitscore = standarize(bitscore)) |>
+  relocate(pid, type, arch_code, arch, length, slength, sbitscore, bitscore)
+
+d_lv <- stringdistmatrix(archs_pid$arch_code, archs_pid$arch_code, method = "lv")
+d_cos <- stringdistmatrix(archs_pid$arch_code, archs_pid$arch_code, method = "cosine")
+d_jac <- stringdistmatrix(archs_pid$arch_code, archs_pid$arch_code, method = "jaccard")
+
+hm <- function(m) {
+  m[1:10, 1:16]
+}
+
+lev <- as_tibble(d_lv) |>
+  mutate(across(everything(), as.integer))
+names(lev) <- archs_pid$pid
+
+lev <- lev |>
+  mutate(
+    pid = archs_pid$pid) |>
+  relocate(pid)
+
+slev <- lev |>
+  mutate(across(!pid, standarize))
+
+slev <- slev |>
+  mutate(
+    slegth = archs_pid$slength,
+    sbitscore = archs_pid$sbitscore,
+    type = archs_pid$type) |>
+  relocate(pid, type)
+
+# https://www.r-bloggers.com/2019/05/quick-and-easy-t-sne-analysis-in-r/
+library(M3C)
+
+sub_num <- slev |>
+  map_lgl(is.numeric)
+slev_numeric <- slev[sub_num]
+
+tsne(slev_numeric, labels=as.factor(slev$type))
+
+# https://datavizpyr.com/how-to-make-tsne-plot-in-r/
+library(Rtsne)
+theme_set(theme_bw(18))
+set.seed(142)
+
+tSNE_fit <- Rtsne(slev_numeric)
+
+tSNE_df <- tSNE_fit$Y %>%
+  as.data.frame() %>%
+  rename(tSNE1="V1",
+         tSNE2="V2") %>%
+  mutate(ID=row_number())
+
+p <- tSNE_df %>%
+  ggplot(aes(x = tSNE1,
+             y = tSNE2,
+             color = type
+  geom_point(alpha = 1/3)+
+  theme(legend.position="bottom")+
+  title("tSNE Bacillota Blast YwqJ, YwqL")
+p
+ggsave(p, "tSNE_proteins_bacillota.svg")
